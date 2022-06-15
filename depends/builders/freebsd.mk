2b803036f6004c46b722cdb126a18e91908c6ebb
Yasser Isa <yasser.isam@gmail.com>
2022-06-15 00:24:42 -0400

    fix curl and rpc test

--- depends/builders/freebsd.mk
+++ depends/builders/freebsd.mk
@@ -1,4 +1,4 @@
 build_freebsd_CC = clang
 build_freebsd_CXX = clang++
 build_freebsd_SHA256SUM = shasum -a 256
-build_freebsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
+build_freebsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -o
